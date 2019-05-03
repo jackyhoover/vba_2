16e4d989f48dd309990288e94bea074226c84ef5
jhoover85 <jackysydnor@gmail.com>
2019-05-02 22:14:45 -0400

    Slight edit to easy code

--- vba_hw_easy.vb
+++ vba_hw_easy.vb
@@ -1,50 +1,46 @@
 sub tickercounter ()
-Dim ws As Worksheets
 
-For Each ws In Worksheets
+    'Define Variables
+    Dim ticker As String
+    Dim ticker_count As Double
+    Dim lastrow As Double
+    Dim summary_row As Double
+    Dim vol As Double
 
-'Define Variables
-Dim ticker As String
-Dim ticker_count As Double
-Dim lastrow As Double
-Dim summary_row As Double
-Dim vol As Double
-
-'Declare initial variables
-lastrow = ws.Cells(Rows.Count, "A").End(xlup).Row
-summary_row = 2
-vol = 0
+    'Declare initial variables
+    lastrow = ws.Cells(Rows.Count, "A").End(xlup).Row
+    summary_row = 2
+    vol = 0
 
 
-'Set headers
-Cells(1, 9).Value = "Ticker"
-Cells(1, 10).Value = "Total Stock Volume"
+    'Set headers
+    Cells(1, 9).Value = "Ticker"
+    Cells(1, 10).Value = "Total Stock Volume"
 
 
-For i = 2 To lastrow
+    For i = 2 To lastrow
 
-    If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
+        If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
 
-        'Set ticker
-        ticker = Cells(i, 1).Value
-        Cells(summary_row, 9).value = ticker
+            'Set ticker
+            ticker = Cells(i, 1).Value
+            Cells(summary_row, 9).value = ticker
 
-        'Set volume
-        vol = vol + Cells(i, 7).Value
-        Cells(summary_row, 10).Value = vol
+            'Set volume
+            vol = vol + Cells(i, 7).Value
+            Cells(summary_row, 10).Value = vol
 
-        summary_row = summary_row + 1
+            summary_row = summary_row + 1
 
-        vol = 0
+            vol = 0
 
-    Else vol = vol + Cells(i, 7).Value  
-            
+        Else vol = vol + Cells(i, 7).Value  
+                
 
-    End If
+        End If
 
     
-Next i
+    Next i
 
-Next ws
 
 End Sub
